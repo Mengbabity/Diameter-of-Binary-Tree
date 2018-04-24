fc57cf3542f2c39fe44fd318bca93cf322284f92
Mengbabity <32949218+Mengbabity@users.noreply.github.com>
2018-04-24 13:54:34 +0800

    Create diameter.cpp

---
+++ diameter.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int depth(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        return 1+max(depth(root->left),depth(root->right));
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)
+            return 0;
+        int res=depth(root->left)+depth(root->right);
+        return max(res,max(diameterOfBinaryTree(root->left),diameterOfBinaryTree(root->right)));
+    }
+};
